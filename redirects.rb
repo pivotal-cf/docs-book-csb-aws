ef29db823c2f7e3e37d5797a379fe86d79feb8d8
RichardJJG <rijohnson@vmware.com>
2022-03-03 12:33:25 +0000

    [#181308931] Update comment to represent new redirections

--- redirects.rb
+++ redirects.rb
@@ -3,7 +3,7 @@ r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.vmware.com' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect from docs.pivotal.io/metrics-forwarder/index.html to docs.vmware.com metrics forwarder docs
+# Redirect from docs.pivotal.io/csb-aws/index.html to docs.vmware.com Cloud Service Broker for AWS docs
 
 r301  %r{/csb-aws/index.html}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
 r301  %r{/csb-aws/}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
