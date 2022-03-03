f647d7b4e7bd5f2c4ad48005faf164405389efc7
RichardJJG <rijohnson@vmware.com>
2022-03-03 12:29:08 +0000

    [#181308931] Changed redirects for CSB for AWS

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,10 @@
 # Redirect all production http traffic to https
-r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
+  rack_env['SERVER_NAME'] == 'docs.vmware.com' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+# Redirect from docs.pivotal.io/metrics-forwarder/index.html to docs.vmware.com metrics forwarder docs
+
+r301  %r{/csb-aws/index.html}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/(.*)}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
