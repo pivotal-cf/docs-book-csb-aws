d6c0d31d6f15aa14f0cafb653226d6560c1fa081
RichardJJG <rijohnson@vmware.com>
2022-03-04 15:59:25 +0000

    [#181308931] Change redirects from staging to live

--- redirects.rb
+++ redirects.rb
@@ -5,6 +5,6 @@ r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
 
 # Redirect from docs.pivotal.io/csb-aws/index.html to docs.vmware.com Cloud Service Broker for AWS docs
 
-r301  %r{/csb-aws/index.html}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
-r301  %r{/csb-aws/}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
-r301  %r{/csb-aws/(.*)}, "https://docs-staging.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/index.html}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/(.*)}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
