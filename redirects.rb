8e2bcbb61f5bcd1d6065a105ec2c9c8211076190
RichardJJG <rijohnson@vmware.com>
2022-03-04 16:03:40 +0000

    [#181308931] Update redirects for VMware migration

--- redirects.rb
+++ redirects.rb
@@ -1,4 +1,4 @@
 # Redirect all production http traffic to https
-r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
-  rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
+r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
+  rack_env['SERVER_NAME'] == 'docs.vmware.com' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
