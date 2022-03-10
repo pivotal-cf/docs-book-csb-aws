f9555b8001b0497dd10ef71c389ec410ed7138d5
RichardJJG <rijohnson@vmware.com>
2022-03-10 19:23:05 +0000

    [#181308931] Add redirects to VMware

--- redirects.rb
+++ redirects.rb
@@ -3,8 +3,9 @@ r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.vmware.com' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirect from docs.pivotal.io/csb-aws/index.html to docs.vmware.com Cloud Service Broker for AWS docs
+# Redirect from docs.pivotal.io/csb-aws/1-1 to docs.vmware.com build service docs
 
-r301  %r{/csb-aws/index.html}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
-r301  %r{/csb-aws/}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
-r301  %r{/csb-aws/(.*)}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-1/index.html}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-1/}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-1}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-1/(.*)}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.1/cloud-service-broker-aws/GUID-$1"
