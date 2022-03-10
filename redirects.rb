2ecd2d9b3d0aa4271a36ab44f0f07421777348a4
RichardJJG <rijohnson@vmware.com>
2022-03-10 19:29:04 +0000

    [#181308931] Add redirects to VMware

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,10 @@
 r301 %r{.*}, 'https://docs.vmware.com$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.vmware.com' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+# Redirect from docs.pivotal.io/csb-aws/1-2 to docs.vmware.com build service docs
+
+r301  %r{/csb-aws/1-2/index.html}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.2/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-2/}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.2/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-2}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.2/cloud-service-broker-aws/GUID-index.html"
+r301  %r{/csb-aws/1-2/(.*)}, "https://docs.vmware.com/en/Cloud-Service-Broker-for-AWS/1.2/cloud-service-broker-aws/GUID-$1"
